9600bcf542b22c2711696984235d327d3623d322
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2023-12-20 22:30:37 +0700

    routing mvc

--- app/Core/App.php
+++ app/Core/App.php
@@ -3,5 +3,17 @@
 class App {
     public function __construct() {
         echo 'OK!';
+
+        $url = $this->parse_url();
+        var_dump($url);
+    }
+
+    public function parse_url() {
+        if(isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
